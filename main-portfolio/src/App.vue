fc9cf419eabd9c51bad84133a191ff90922de4eb
huzeir2k <huzeir2k@gmail.com>
2025-06-23 15:19:02 -0400

    again grand

---
+++ main-portfolio/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import IndexPage from './components/IndexPage.vue';
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+  </header>
+
+  <main>
+    <IndexPage />
+  </main>
+</template>
+
+
